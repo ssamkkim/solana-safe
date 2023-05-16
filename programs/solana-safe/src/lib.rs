dab2a93d8bfc280e1602655f503610a0107000b8
ssamkkim <ssamuel.kkim@gmail.com>
2023-05-15 21:09:03 -0700

    initialize safe instruction

--- programs/solana-safe/src/lib.rs
+++ programs/solana-safe/src/lib.rs
@@ -6,13 +6,29 @@ declare_id!("6vEvbFSmX4gabvWUf5TrAbU3Lw1B2AnKPtRS2kfefnEx");
 pub mod solana_safe {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize_safe(ctx: Context<InitializeSafe>, name: String) -> Result<()> {
+        let safe: &mut Account<Safe> = &mut ctx.accounts.safe;
+        let owner: &Signer = &ctx.accounts.owner;
+
+        if name.chars().count() > 40 {
+            return Err(error!(ErrorCode::NameTooLong))
+        }
+
+        safe.owner = *owner.key;
+        safe.name = name;
+        safe.balance = 0;
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct InitializeSafe<'info> {
+    #[account(init, payer = owner, space = Safe::LEN)]
+    pub safe: Account<'info, Safe>,
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
 
 #[account]
 pub struct Safe {
@@ -32,4 +48,10 @@ impl Safe {
         + PUBLIC_KEY_LENGTH // Owner
         + STRING_LENGTH_PREFIX + MAX_NAME_LENGTH // Name
         + BALANCE_LENGTH; // Balance
+}
+
+#[error_code]
+pub enum ErrorCode  {
+    #[msg("The provided name should be 40 characters long maximum.")]
+    NameTooLong,
 }
