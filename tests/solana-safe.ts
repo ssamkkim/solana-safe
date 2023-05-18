4374315bfe70296f7ae1b1b2bfcd1bc709d2e49e
ssamkkim <ssamuel.kkim@gmail.com>
2023-05-17 23:22:50 -0700

    initialize safe test

--- tests/solana-safe.ts
+++ tests/solana-safe.ts
@@ -1,16 +1,32 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
+import * as assert from "assert";
+import * as bs58 from "bs58";
 import { SolanaSafe } from "../target/types/solana_safe";
 
 describe("solana-safe", () => {
   // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
+  const program = anchor.workspace.SolanaSafe;
 
-  const program = anchor.workspace.SolanaSafe as Program<SolanaSafe>;
+  it("can initialize a safe", async () => {
+    const safe = anchor.web3.Keypair.generate();
+    await program.rpc.initializeSafe('test safe', {
+      accounts: {
+        safe: safe.publicKey,
+        owner: program.provider.wallet.publicKey,
+        systemProgram: anchor.web3.SystemProgram.programId,
+      },
+      signers: [
+        safe,
+      ],
+    });
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+    const safeAccount = await program.account.safe.fetch(safe.publicKey);
+
+    assert.equal(safeAccount.owner.toBase58(), program.provider.wallet.publicKey.toBase58());
+    assert.equal(safeAccount.name, "test safe");
+    assert.equal(safeAccount.balance, 0);
   });
 });
